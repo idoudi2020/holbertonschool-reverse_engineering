d189187c19f4f985a751bec6e9fd6aa2f6a5f80f
idoudi2020 <idoudimokhtar@gmail.com>
2025-03-05 16:09:21 -0600

    script to display the extracted information in a specific format

--- re_fundamentals/get_entry_point.sh
+++ re_fundamentals/get_entry_point.sh
@@ -1,40 +1,38 @@
 #!/bin/bash
 
-# Charger le script de messages
 source ./messages.sh
 
-# Vérifier qu'un seul argument est fourni
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <elf_file>"
-    exit 1
+# one command line arg
+if [ $# -ne 1 ]; then
+	echo "Usage: $0 <elf_file>"       
+	exit 1
 fi
 
-# Vérifier si le fichier existe
-file_name="$1"
-if [ ! -f "$file_name" ]; then
-    echo "Error: '$file_name' is not a valid file."
-    exit 1
+# arg is a file
+if [ ! -f "$1" ]; then
+	echo "error not a file"
+	exit 1
 fi
 
-# Vérifier si le fichier est un ELF
-if ! file "$file_name" | grep -q "ELF"; then
-    echo "Error: '$file_name' is not an ELF file."
-    exit 1
+# is type: elf file
+if ! file "$1" | grep -q "ELF"; then
+	echo "not an elf file"
+	exit 1
 fi
 
-# Extraire les informations de l'en-tête ELF avec readelf
-magic_number=$(readelf -h "$file_name" | awk '/Magic:/ {print substr($0, index($0,$2))}')
-class=$(readelf -h "$file_name" | awk '/Class:/ {print $2}')
-byte_order=$(readelf -h "$file_name" | awk -F, '/Data:/ {print $2}' | xargs)
-entry_point_address=$(readelf -h "$file_name" | awk '/Entry point address:/ {print $4}')
+elf_magic=$(readelf -h "$1" | grep "Magic:" | awk '{$1=""; print $0}')
+elf_class=$(readelf -h "$1" | grep "Class:" | awk '{$1=""; print $0}')
+elf_byte_order=$(readelf -h "$1" | grep "Data:" | awk '{$1=""; print $0}')
+elf_entry_point=$(readelf -h "$1" | grep "Entry point address:" | awk '{$1=""; $2=""; $3=""; print $0}')
 
-# Exporter les variables pour messages.sh
-export file_name
-export magic_number
-export class
-export byte_order
-export entry_point_address
+arg1=$(echo "${elf_magic:1}")
+arg2=$(echo "${elf_class:1}")
+arg3=$(echo "${elf_byte_order:17}")
+arg4=$(echo "${elf_entry_point:3}")
 
-# Afficher les informations formatées
+export file_name="$1"
+export magic_number="$arg1"
+export class="$arg2"
+export byte_order="$arg3"
+export entry_point_address="$arg4"
 display_elf_header_info
-
